1f9b669879265e5fec5783fabbc969b0381e8cd8
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2015-03-18 10:13:35 -0700

    Add GPLv3 license

--- meggyjr-cncserver.py
+++ meggyjr-cncserver.py
@@ -3,6 +3,22 @@ import cncserverclient
 import argparse
 import sys
 
+# Copyright (C) 2015  Schuyler St. Leger
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("cncserver")
 parser.add_argument("serialport")
