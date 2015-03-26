87294f1b675dd2c6511cd82cc8c151d9238da606
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2015-03-26 08:51:03 -0700

    Add function to unlock motors

--- cncserverclient.py
+++ cncserverclient.py
@@ -122,6 +122,15 @@ class CNCServerClient:
             # Ignore timeouts on the returned status
             pass
 
+    def unlockMotors(self):
+        try:
+            # Send the request to the server
+            r = requests.delete(self.cncserver_address + '/v1/motors/', timeout = 0.01)
+        except requests.exceptions.ReadTimeout: pass
+        except requests.exceptions.ConnectTimeout:
+            # Ignore timeouts on the returned status
+            pass
+
     def launchCncServer(self):
         """
         Looks for a built-in CNCServer instance at ../cncserver/ and launches it.
